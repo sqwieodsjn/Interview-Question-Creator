e5e900989e597b4183bcb4fb8637d2006561a683
Shibin T <shibint283@gmail.com>
2026-05-29 16:42:24 +0530

    Applicable for all types of the files

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import os
 
 app = FastAPI(title = "Interview Question Creatot",
@@ -9,20 +9,32 @@ app = FastAPI(title = "Interview Question Creatot",
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+ALLOWED_EXTENSIONS = {
+    ".pdf",
+    ".txt",
+    ".docx",
+    ".png",
+    ".jpg",
+    ".jpeg"
+}
+
 @app.get("/")
 def home():
     return {
         "message": "Interview Question Creator API is running!"
     }
 
-@app.post("/upload-pdf")
+@app.post("/upload-file")
 async def upload_pdf(file:UploadFile = File(...)):
-
+    file_extension = os.path.splitext(file.filename)[1].lower()
+    if file_extension not in ALLOWED_EXTENSIONS:
+        raise HTTPException(status_code=400, detail="Unsupported file type. Allowed types: {', '.join(ALLOWED_EXTENSIONS)}")
     file_path = os.path.join(UPLOAD_DIR, file.filename)
+    content = await file.read()
     with open(file_path, "wb") as f:
-        content = await file.read()
         f.write(content)
     return {
         "filename": file.filename,
+        "file_type": file_extension,
         "message": "File uploaded successfully!"
     }
