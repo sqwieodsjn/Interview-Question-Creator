ebe72a54aa4f27f552f1370cf923f6ca0f0d7ab8
Shibin T <shibint283@gmail.com>
2026-05-29 16:30:35 +0530

    Phase3: PDF upload functionality

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import os
 
 app = FastAPI(title = "Interview Question Creatot",
               description = "Generate interview questions from  PDFs using LangChain and FAISS",
               version = "1.0.0"
 )
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def home():
@@ -11,8 +15,14 @@ def home():
         "message": "Interview Question Creator API is running!"
     }
 
-@app.get("/health")
-def health_check():
+@app.post("/upload-pdf")
+async def upload_pdf(file:UploadFile = File(...)):
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    with open(file_path, "wb") as f:
+        content = await file.read()
+        f.write(content)
     return {
-        "status": "healthy"
+        "filename": file.filename,
+        "message": "File uploaded successfully!"
     }
